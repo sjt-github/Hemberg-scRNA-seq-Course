f11b59ea526b0a9d96c68953f665e74610cd4ae7
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2016-12-13 16:30:48 +0000

    create pdf

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
 
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-# bookdown::render_book("index.Rmd", "bookdown::pdf_book", force_knit = TRUE)
+# bookdown::render_book("index.Rmd", "bookdown::gitbook")
+bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 # bookdown::render_book("index.Rmd", "bookdown::epub_book")
