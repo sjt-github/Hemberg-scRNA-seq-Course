edf2fbe291307db8e784c3c3ab9366eb3107c3ff
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-03-09 17:06:02 +0000

    remove travis yaml; update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ docker pull hemberglab/scrna.seq.course:latest
 docker run hemberglab/scrna.seq.course:latest
 
 # copy files from the docker
-rm -r docs
 alias dl='docker ps -l -q'
 docker cp `dl`:_book $WORKSPACE/docs
 
@@ -26,22 +25,5 @@ git commit -m "update the course website"
 # git push origin master
 
 # cleanup after docker usage
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
-
-
-
-# 
-# # clone the repository to the book-output directory
-# git clone -b gh-pages \
-#   https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
-#   book-output
-# cd book-output
-# mkdir blischak
-# cp ../blischak/umi.rds blischak/
-# cp ../blischak/reads.rds blischak/
-# cp -r ../_book/* ./
-# rm -rf _bookdown_files
-# git add *
-# git commit -m "Build the book"
-# git push origin HEAD:gh-pages
+docker rm `dl`
+docker rmi hemberglab/scrna.seq.course:latest
